8cf9ffc6fc992d842e2ece4c5c633c3766557cb9
cazmusw <wcazmusw@gmail.com>
2025-06-03 22:50:04 +0900

    Test

--- builder/__main__.py
+++ builder/__main__.py
@@ -3,55 +3,55 @@ import DownloadManager
 
 def main():
 
-    downloadDrivers = input("Скачать драйвера? (y = yes) ") == 'y'
-    downloadPrograms = input("Скачать мои программы? (y = yes) ") == 'y'
-
-
-    print("Installation Start")
-
-    DownloadManager.__gnome_optimizer()
-    DownloadManager.__clear_gnome()
-    DownloadManager.__updater_system()
-
-    #Включение мультибиблиотек
-    DownloadManager.__patch_multilib()
-
-    #Выключение таймаута
-    DownloadManager.__disable_timeout()
-
-    #Устанавливаем первые пакеты для дальнейшей быстрой установки
-    DownloadManager.__install_pacman_package(packages.FAST_PACKAGES)
-    DownloadManager.__optimize_mirrors()
-
-    #Устанавливаем пакеты базовые
-    DownloadManager.__install_pacman_package(packages.BASE_PACKAGES)
-
-    if downloadDrivers:
-        # Микрокод установка
-        DownloadManager.__install_microcode()
-        DownloadManager.__install_pacman_package(packages.MY_DRIVERS)
-
-    if downloadPrograms:
-        DownloadManager.__install_pacman_package(packages.MY_PROGRAMS)
-
-    #Disk cleaner
-    DownloadManager.__disk_optimizer()
-    #Optimize system
-    DownloadManager.__irqbalance_integration()
-    DownloadManager.__init_build_optimizer()
-
-
-    input("Остановочная точка, далее потребуется подтверждать установку, будьте у экрана")
-
-    #Task manager
-    DownloadManager.__ananicy_cpp_integration()
-    #Установка аур и его пакетов
-    DownloadManager.__install_yay()
-    if downloadPrograms:
-        DownloadManager.__install_aur_package(packages.AUR_PACKAGES)
-    DownloadManager.__install_sound_optimizer()
-    DownloadManager.__pacman_cash_cleaner()
-
+    # downloadDrivers = input("Скачать драйвера? (y = yes) ") == 'y'
+    # downloadPrograms = input("Скачать мои программы? (y = yes) ") == 'y'
+    #
+    #
+    # print("Installation Start")
+    #
+    # DownloadManager.__gnome_optimizer()
+    # DownloadManager.__clear_gnome()
+    # DownloadManager.__updater_system()
+    #
+    # #Включение мультибиблиотек
+    # DownloadManager.__patch_multilib()
+    #
+    # #Выключение таймаута
+    # DownloadManager.__disable_timeout()
+    #
+    # #Устанавливаем первые пакеты для дальнейшей быстрой установки
+    # DownloadManager.__install_pacman_package(packages.FAST_PACKAGES)
+    # DownloadManager.__optimize_mirrors()
+    #
+    # #Устанавливаем пакеты базовые
+    # DownloadManager.__install_pacman_package(packages.BASE_PACKAGES)
+    #
+    # if downloadDrivers:
+    #     # Микрокод установка
+    #     DownloadManager.__install_microcode()
+    #     DownloadManager.__install_pacman_package(packages.MY_DRIVERS)
+    #
+    # if downloadPrograms:
+    #     DownloadManager.__install_pacman_package(packages.MY_PROGRAMS)
+    #
+    # #Disk cleaner
+    # DownloadManager.__disk_optimizer()
+    # #Optimize system
+    # DownloadManager.__irqbalance_integration()
+    # DownloadManager.__init_build_optimizer()
+    #
+    #
+    # input("Остановочная точка, далее потребуется подтверждать установку, будьте у экрана")
+    #
+    # #Task manager
+    # DownloadManager.__ananicy_cpp_integration()
+    # #Установка аур и его пакетов
+    # DownloadManager.__install_yay()
+    # if downloadPrograms:
+    #     DownloadManager.__install_aur_package(packages.AUR_PACKAGES)
+    # DownloadManager.__install_sound_optimizer()
+    # DownloadManager.__pacman_cash_cleaner()
+    #
 
     #Post install
     DownloadManager.__change_gnome_settings_to_my()
