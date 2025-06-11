7309beac48e6591a3a9dc4ab29f56dc0cd8bcb7e
user <user@user>
2025-06-12 05:27:37 +0900

    .

--- builder/Utils.py
+++ builder/Utils.py
@@ -7,7 +7,7 @@ def __install_yay():
 
 
 def __optimize_mirrors():
-    os.system("sudo reflector --verbose -l 25 --sort rate --save /etc/pacman.d/mirrorlist")
+    os.system("sudo reflector --verbose --country 'Russia' -l 25 --sort rate --save /etc/pacman.d/mirrorlist")
 
 def __install_pacman_package(package_names: list):
     for package in package_names:
